0f961b58448207cb971aec893d4afe31a0be7788
Mostradamus <sebastianfloresgrandez17@gmail.com>
2025-01-22 10:53:31 -0500

    new

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/Login/Login.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/Home/Home.component';
-import { LoginGuard } from './utils/logeo.guard';
-import { LogeoGuard } from './utils/login.guard';
+import { LoginGuard } from './Utils/logeo.guard';
+import { LogeoGuard } from './Utils/login.guard';
 
 export const routes: Routes = [
   { path: 'Login', component: LoginComponent, canActivate:[LoginGuard] },
