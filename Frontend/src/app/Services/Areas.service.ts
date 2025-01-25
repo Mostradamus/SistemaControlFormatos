0df44c5c287ca4dd63d0ecaa5ebeb377486f636f
Mostradamus <sebastianfloresgrandez17@gmail.com>
2025-01-24 22:59:25 -0500

    ahora mismo

---
+++ Frontend/src/app/Services/Areas.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../Environment/Environment';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AreasService {
+  private myAppUrl: string;
+  public myApiUrl: string;
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.api;
+    this.myApiUrl = 'api/v1/areas';
+  }
+  GetInfoAreas(): Observable<any> {
+    return this.http.get(`${this.myAppUrl}${this.myApiUrl}`);
+  }
+}
