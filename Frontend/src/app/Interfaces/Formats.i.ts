6a5cf3054fceb2e2113232ce87cc7b973a7dcfe5
Mostradamus <sebastianfloresgrandez17@gmail.com>
2025-01-24 23:10:24 -0500

    ahurita mismo

--- Frontend/src/app/Interfaces/Formats.i.ts
+++ Frontend/src/app/Interfaces/Formats.i.ts
@@ -1,9 +1,10 @@
 export interface Formats {
   id_formats?: number;
   status?: number;
+  id_area?: number;
   starting_order?: string;
   total?: number;
   registration_date?: Date;
-  turn?: number;
+  id_turn?: number;
   description?: string;
 }
