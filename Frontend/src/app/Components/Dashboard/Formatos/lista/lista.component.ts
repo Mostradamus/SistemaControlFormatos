50cd9353c6f0affeff7147bd4ac585be90493fc9
pomaDevOficial <132797471+pomaDevOficial@users.noreply.github.com>
2025-01-27 23:37:35 -0500

    add

---
+++ Frontend/src/app/Components/Dashboard/Formatos/lista/lista.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+import { sp_mostrar_formats } from '../../../../Interfaces/sp_mostrar_formats';
+import { FormatsService } from '../../../../Services/Formats.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css'],
+  imports:[TableModule, CommonModule],
+  providers: [FormatsService]
+})
+export default class ListaComponent implements OnInit {
+
+  public dataF!: sp_mostrar_formats[];
+   private _F = inject(FormatsService)
+   ngOnInit() {
+     this._F.getAllFormatsSp().subscribe((data: sp_mostrar_formats[])=>{
+       this.dataF = data;
+     })
+    
+   }
+}
