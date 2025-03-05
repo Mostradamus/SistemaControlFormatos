21bf03f58828d6e0246f786cc27bf0f9f84de445
pomaDevOficial <132797471+pomaDevOficial@users.noreply.github.com>
2025-03-04 23:12:51 -0500

    ADDD

--- Frontend/src/app/Components/Dashboard/Home/Home.component.ts
+++ Frontend/src/app/Components/Dashboard/Home/Home.component.ts
@@ -4,18 +4,21 @@ import { Users } from '../../../Interfaces/Users.i';
 import {CardModule} from 'primeng/card'
 import {RippleModule} from 'primeng/ripple'
 import {ButtonModule} from 'primeng/button'
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TabsModule } from 'primeng/tabs';
 @Component({
   selector: 'app-Home',
   templateUrl: './Home.component.html',
   styleUrls: ['./Home.component.css'],
-  imports: [CardModule,RippleModule],
+  imports: [CardModule,RippleModule, ScrollPanelModule, TabsModule],
   providers:[UsersService]
 })
 export default class HomeComponent implements OnInit {
 
+  listaServicios: string [] = []
   ngOnInit() {
-   
+   this.listaServicios = [ "MEDICINA GENERAL", "OBSTETRICIA", "LABORATORIO", "CONTROL DE NIÑO", "FARMACIA", "TBC", "DENGUE", "ECOGRAFIA", "ADMISION", "CAJA"]
   }
- 
+  
 
 }
